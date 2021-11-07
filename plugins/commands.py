0e0a51d552dfc2ab8309288ba7d983b84cc4a6d7
Mufaz-TG <93363575+BXBotz2021@users.noreply.github.com>
2021-11-07 12:21:11 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -175,4 +175,4 @@ async def delete_all_index_confirm(bot, message):
 
 @Client.on_message(filters.private | filters.group & filters.command("dice"))
 async def dice(bot, message):
-    await bot.send_dice("dice", "🎲")
+    await message.reply_text("🎲")
