b3d31c44f873fbb2300b76d76a3a87c074225a1a
Mufaz-TG <93363575+BXBotz2021@users.noreply.github.com>
2021-11-17 09:34:40 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+  git clone https://github.com/BXBotz2021/Movie-Bot.git /Movie-Bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /Movie-Bot
 fi
-cd /EvaMaria
+cd /Movie-Bot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
-python3 bot.py
+/bin/bash /start.sh
