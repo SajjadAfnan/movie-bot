2ebca2115635b0d5a76916b3a3c1cdb18ca3f917
Mufaz-TG <93363575+BXBotz2021@users.noreply.github.com>
2022-05-25 06:09:24 +0530

    Update info.py

--- info.py
+++ info.py
@@ -36,6 +36,12 @@ DATABASE_URI = environ.get('DATABASE_URI', "")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Rajappan")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
+Mongo #2
+
+DATABASE_URI_2 = environ.get('DATABASE_URI_2', "")
+DATABASE_NAME_2 = environ.get('DATABASE_NAME_2', "Ava)
+COLLECTION_NAME_2 = environ.get('COLLECTION_NAME_2', 'Telegram_files2')
+
 # Others
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'TeamEvamaria')
