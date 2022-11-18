8ef1cb61cc7fc83c5d0d797359fe02d164f0e8ec
Mufaz-TG <93363575+BXBotz2021@users.noreply.github.com>
2022-11-18 19:19:11 +0530

    Update sample_info.py

--- sample_info.py
+++ sample_info.py
@@ -1,24 +1,29 @@
 # Bot information
 SESSION = 'Media_search'
 USER_SESSION = 'User_Bot'
-API_ID = 12345
-API_HASH = '0123456789abcdef0123456789abcdef'
-BOT_TOKEN = '123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11'
+API_ID = 4088138
+API_HASH = '4b19fd7339c360e956461f0f5535c5b1'
+BOT_TOKEN = '2132307754:AAEr-fTC2q7uNP6BUFhfRaw5_em_Ocek_PA'
 USERBOT_STRING_SESSION = ''
 
 # Bot settings
 CACHE_TIME = 300
-USE_CAPTION_FILTER = False
+USE_CAPTION_FILTER = True
 
 # Admins, Channels & Users
-ADMINS = [12345789, 'admin123', 98765432]
-CHANNELS = [-10012345678, -100987654321, 'channelusername']
+ADMINS = [1890385137, 292920472]
+CHANNELS = [-1001607869896, -1001363459378, '-1001607869896']
 AUTH_USERS = []
 AUTH_CHANNEL = None
 
 # MongoDB information
-DATABASE_URI = "mongodb://[username:password@]host1[:port1][,...hostN[:portN]][/[defaultauthdb]?retryWrites=true&w=majority"
-DATABASE_NAME = 'Telegram'
-COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each bot
+DATABASE_URI = "mongodb+srv://BXBotz:BXBotz@cluster0.5a4uk.mongodb.net/?retryWrites=true&w=majority"
+DATABASE_NAME = 'MisukaBot'
+COLLECTION_NAME = 'Telegram_files'  # If you are using the same database, then use different collection name for each bot
 
+CUSTOM_FILE_CAPTION = '<code>{file_name}</code>'
+IMDB = 'True'
+PICS = 'https://telegra.ph/file/0caa846490468b52352ae.jpg https://telegra.ph/file/0086063f7b38b6ba7aa31.jpg'
+SINGLE_BUTTON = 'True'
+P_TTI_SHOW_OFF = 'True'
 
